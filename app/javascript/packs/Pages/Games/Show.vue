56982b5ec38a53e88af67d7c81e5a2ee2d626bd4
Nevin Chen <38507731+Nevin-Chen@users.noreply.github.com>
2022-06-30 18:03:26 -0400

    adds tailwind with postcss (#5)

--- app/javascript/packs/Pages/Games/Show.vue
+++ app/javascript/packs/Pages/Games/Show.vue
@@ -1,20 +1,22 @@
 <template>
     <div>
-    <h1>{{ game.title }}</h1>
-        <table>
+    <h1 class="inline-block text-3xl font-extrabold text-gray-900 m-4">{{ game.title }}</h1>
+        <table class="table-auto">
             <thead>
                 <tr>
-                    <th>Genre</th>
-                    <th>Platform</th>
-                    <th>Actions</th>
+                    <th class="p-4">Genre</th>
+                    <th class="p-4">Platform</th>
+                    <th class="p-4">Actions</th>
                 </tr>
             </thead>
             <tbody>
                 <tr :key="game.id">
-                    <td><span >{{ game.genre }}</span></td>
-                    <td><span >{{ game.platform }}</span></td>
-                    <td><Link :href="'/games/' + game.id + '/edit'">Edit</Link></td>
-                    <td><Link :href="'/games/' + game.id" method="delete" as="button" type="button">Remove</Link></td>
+                    <td class="p-4"><span >{{ game.genre }}</span></td>
+                    <td class="p-4"><span >{{ game.platform }}</span></td>
+                    <td class="p-4">
+                        <Link class="inline-flex justify-center rounded-lg text-sm font-semibold py-1.5 px-2.5 bg-gray-500 text-white hover:bg-gray-300 -my-2.5 mr-1" :href="'/games/' + game.id + '/edit'">Edit</Link>
+                        <Link class="inline-flex justify-center rounded-lg text-sm font-semibold py-1.5 px-2.5 bg-pink-500 text-white hover:bg-pink-300 -my-2.5" :href="'/games/' + game.id" method="delete" as="button" type="button">Remove</Link>
+                    </td>
                 </tr>
             </tbody>
         </table>
--- app/javascript/packs/Pages/Home.vue
+++ app/javascript/packs/Pages/Home.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <h1>Games Directory</h1>
+        <div class="relative flex items-center">
+          <h1 class="inline-block text-3xl font-extrabold text-gray-900 m-4">Games Directory</h1>
           <form @submit.prevent="submit">
-						<button type="submit" :disabled="submit.processing">Generate game</button>
+						<button class="pointer-events-auto rounded-md bg-blue-900 m-3 py-4 px-5 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-blue-700" type="submit" :disabled="submit.processing">Generate game</button>
 					</form>
-        <div v-for="game in games" :key="game.id">
-            <h3><Link :href="'/games/' + game.id">{{ game.title }}</Link></h3>
-            <div><Link :href="'/games/' + game.id" method="delete" as="button" type="button">Remove</Link></div>
+        </div>
+        <div class="relative flex items-center" v-for="game in games" :key="game.id">
+            <h3><Link class="px-6 py-4 flex items-center focus:text-blue-700 hover:underline" :href="'/games/' + game.id">{{ game.title }}</Link></h3>
+            <div><Link class="inline-flex justify-center rounded-lg text-sm font-semibold py-1.5 px-2.5 bg-pink-500 text-white hover:bg-pink-300 -my-2.5" :href="'/games/' + game.id" method="delete" as="button" type="button">Remove</Link></div>
         </div>
         <Pagination :pagy="pagy"/>
     </div>
